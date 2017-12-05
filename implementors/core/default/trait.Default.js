5a8fd65da52aed2d13044b82a43564cad9624e9a
Travis CI <dnaq@users.noreply.github.com>
2017-12-05 16:33:55 +0000

    Deploy to GitHub Pages

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["serde"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"serde/de/struct.IgnoredAny.html\" title=\"struct serde::de::IgnoredAny\">IgnoredAny</a>",];
+implementors["sodiumoxide"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"sodiumoxide/crypto/hash/sha512/struct.State.html\" title=\"struct sodiumoxide::crypto::hash::sha512::State\">State</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"sodiumoxide/crypto/hash/sha256/struct.State.html\" title=\"struct sodiumoxide::crypto::hash::sha256::State\">State</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
